821c66d3fef28e2ea212b976aeb5cf6eb863fa6f
Bichwaa <ptonny13@gmail.com>
2026-03-15 15:49:39 +0300

    add backend script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
       name: "bidsconnect-back",
       cwd: "/var/www/bidsconnect",
       script: "bin/gunicorn",
-      args: "bidsconnectBack.wsgi:application \
+      args: "bidsconnect.wsgi:application \
              --bind 127.0.0.1:8000 \
              --workers 3 \
              --timeout 120 \
